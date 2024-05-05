ea45cebb6a59552d5acddbec497184b7897891b5
zhangwei <zhangwei19890518@gmail.com>
2024-05-05 21:45:05 +0800

    complete zwconfig test demo for two means of configuration

--- src/main/java/io/github/zhangwei1989/zwconfig/demo/ZwconfigDemoApplication.java
+++ src/main/java/io/github/zhangwei1989/zwconfig/demo/ZwconfigDemoApplication.java
@@ -1,5 +1,6 @@
 package io.github.zhangwei1989.zwconfig.demo;
 
+import io.github.zhangwei1989.zwconfig.client.annotation.EnableZWConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
@@ -10,24 +11,25 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ZWDemoConfig.class})
+@EnableZWConfig
 public class ZwconfigDemoApplication {
 
-	@Value("${zw.a}")
-	private String a;
+    @Value("${zw.a}")
+    private String a;
 
-	@Autowired
-	private ZWDemoConfig zwDemoConfig;
+    @Autowired
+    private ZWDemoConfig zwDemoConfig;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZwconfigDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZwconfigDemoApplication.class, args);
+    }
 
-	@Bean
-	ApplicationRunner applicationRunner() {
-		return args -> {
-			System.out.println(a);
-			System.out.println(zwDemoConfig.getA());
-		};
-	}
+    @Bean
+    ApplicationRunner applicationRunner() {
+        return args -> {
+            System.out.println(a);
+            System.out.println(zwDemoConfig.getA());
+        };
+    }
 
 }
